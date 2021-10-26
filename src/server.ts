02942d186e0c445a9144a2208a8e9d58af1988d1
riittagerhards <rgerhards@outlook.com>
2021-10-26 11:37:47 +0200

    ✨ Add new request

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,15 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.get('/api/users/:name', (request, response) => {
+  response.send(request.params.name);
+});
+
+app.get('/api/users', (_request, response) => {
+  const users = ['Mickey', 'Donald', 'Goofy'];
+  response.send(users);
+});
+
 app.get('/', (_req, res) => {
   res.send('Hello World!');
 });
