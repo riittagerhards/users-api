321b6103725223701cfc6d1aefdb6862114d008d
riittagerhards <rgerhards@outlook.com>
2021-10-28 11:31:45 +0200

    ✨ Add .env

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import { connectDatabase } from './utils/database';
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('No MongoDB URI dotenv variable');
+}
 
 const app = express();
 const port = 3000;
@@ -113,6 +121,8 @@ app.get('/', (_req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+connectDatabase(process.env.MONGODB_URI).then(() =>
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  })
+);
